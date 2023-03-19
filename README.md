049844a3e2b88bd0da57d69b1e01c0dac9120973
cel <cel@cenics.cat>
2023-03-19 19:55:31 +0100

    https

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ https://next.serverscel.com
 cel@cenics.cat 
 1gYUUTpuJ4EsJVAx1RF
 ```
-
+https://next.serverscel.com/
 
 https://sql.serverscel.com/phpmyadmin34rdsf/index.php
---
+++ server.js
@@ -0,0 +1,28 @@
+// server.js
+const { createServer } = require('http');
+const next = require('next');
+const absoluteUrl = require('next-absolute-url').default;
+const { parse } = require('url');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => createServer((req, res) => {
+  const { url } = req;
+  const { protocol, host } = absoluteUrl(req);
+
+  if (!dev && protocol === 'http:') {
+    res.writeHead(301, {
+      Location: `https://${host}${url}`
+    });
+    res.end();
+    return {};
+  }
+
+  const parsedUrl = parse(url, true);
+  return handle(req, res, parsedUrl);
+}).listen(3060, (err) => {
+  if (err) throw err;
+  console.log('> Sever is running!');
+}));
