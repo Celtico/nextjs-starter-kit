aae27385355096cae110228e69b64fc35a3aaa46
cel <cel@cenics.cat>
2023-03-19 19:12:30 +0100

    init deploy

---
+++ README.md
@@ -0,0 +1,9 @@
+LOGIN
+===========
+```
+http://localhost:3000/account 
+cel@cenics.cat 
+1gYUUTpuJ4EsJVAx1RF
+```
+
+https://sql.serverscel.com/phpmyadmin34rdsf/index.php
--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,10 +1,10 @@
-import { NextResponse, NextRequest, NextMiddleware } from "next/server";
+import { NextResponse } from "next/server";
 
 // https://beta.nextjs.org/docs/api-reference/response
 export async function GET(request: Request) {
-  let obj = { x: 1, y: 2, z: 3 };
+//  let obj = { x: 1, y: 2, z: 3 };
 
-  console.log(JSON.stringify(obj, null, 2)); // spacing level = 2
+  //console.log(JSON.stringify(obj, null, 2)); // spacing level = 2
 
   // return  JSON.stringify(request.url,null,2)
   // return NextResponse.redirect(new URL('/new', request.url));
